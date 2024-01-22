3de341914de3785d7b2008d3734683c3334a1a05
DevDior <chinup1004@naver.com>
2024-01-22 23:56:44 +0900

    Section2 ~ API

--- 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,10 @@
 package hello.hellospring.controller;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,35 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("spring")
-    public String spring(@NotNull Model model) {
-        model.addAttribute("data", "spring!!");
-        return "spring";
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
